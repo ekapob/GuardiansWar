9f8e63d484013610efe8a7fe5eb5892dd0f5b138
ekapob <35923406+ekapob@users.noreply.github.com>
2018-02-05 18:28:09 +0700

    Kick player
    while playing if some player quit the game anyone will be kicked by server

--- Assets/Scripts/Extra/PauseAndExitButton.cs
+++ Assets/Scripts/Extra/PauseAndExitButton.cs
@@ -7,13 +7,13 @@ public class PauseAndExitButton : MonoBehaviour {
 	public static PauseAndExitButton Instance;
 	public GameObject camSpawnPoint;
 	public GameObject allButton;
+	public GameObject exitButton;
 	public bool pause;
 	// Use this for initialization
 	void Start () {
 		Instance = this;
 		pause = false;
 	}
-	
 
 	public void OnClickPauseSelect(){
 		allButton.SetActive (true);
--- Assets/Scripts/Gameplay/Canvas/CanvasGameplayControl.cs
+++ Assets/Scripts/Gameplay/Canvas/CanvasGameplayControl.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CanvasGameplayControl : MonoBehaviour {
 
 	public GameObject standbyCam;
+	private int allPlayerInGame;
 	// Use this for initialization
 	void Start () {
+		allPlayerInGame = PhotonNetwork.playerList.Length;
 		standbyCam.SetActive (true);
 	}
 	
@@ -14,6 +16,11 @@ public class CanvasGameplayControl : MonoBehaviour {
 	void Update () {
 		if (PhotonNetwork.connectionStateDetailed.ToString () == "Joined") {
 			standbyCam.SetActive (false);
+		}
+		if (allPlayerInGame != PhotonNetwork.playerList.Length) {
+			PauseAndExitButton.Instance.pause = true;
+			PauseAndExitButton.Instance.allButton.SetActive (true);
+			PauseAndExitButton.Instance.exitButton.SetActive (false);
 		}
 	}
 }
