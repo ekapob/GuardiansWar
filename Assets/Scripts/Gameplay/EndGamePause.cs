ade1a218d2f5db2bd1109fed789bcd98790f54f6
ponpawit <ponpawit@users.noreply.github.com>
2018-03-15 07:43:04 +0700

    EndGame
    -Add EndGamePause

---
+++ Assets/Scripts/Gameplay/EndGamePause.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGamePause : MonoBehaviour {
+
+	void Start () 
+	{
+		Time.timeScale = 0f;
+	}
+}
--- Assets/Scripts/Gameplay/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemy.cs
@@ -7,6 +7,23 @@ public class Enemy : MonoBehaviour {
 
 	Manager manager;
 
+	//-----
+
+	private Transform target;
+
+	private float range = 2.5f;
+
+	public GameObject enemyAttackPrefab;
+
+	public float fireRate = 1f;
+	private float fireCountdown = 0f;
+
+	public string enemyTag = "Core";
+
+	public Transform firePoint;
+
+	//-----
+
 	public float startSpeed = 10f;
 	public float speed;
 
@@ -23,6 +40,63 @@ public class Enemy : MonoBehaviour {
 		currentHealth = startHealth;
 
 		speed = startSpeed;
+
+		InvokeRepeating("UpdateTarget", 0f, 0.5f);
+	}
+
+	void UpdateTarget ()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+		float shortestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+		foreach (GameObject enemy in enemies)
+		{
+			float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+			if (distanceToEnemy < shortestDistance)
+			{
+				shortestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+		}
+
+		if (nearestEnemy != null && shortestDistance <= range)
+		{
+			target = nearestEnemy.transform;
+		} else
+		{
+			target = null;
+		}
+	}
+
+	void Update () {
+		if (target == null) 
+		{
+			return;
+		}
+
+		LockOnTarget();
+
+		if (fireCountdown <= 0f) {
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		}
+
+		fireCountdown -= Time.deltaTime;
+	}
+
+	void LockOnTarget()
+	{
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation(dir);
+	}
+
+	void Shoot ()
+	{
+		GameObject bulletGO = (GameObject)Instantiate(enemyAttackPrefab, firePoint.position, firePoint.rotation);
+		EnemyAttack bullet = bulletGO.GetComponent<EnemyAttack>();
+
+		if (bullet != null)
+			bullet.Seek(target);
 	}
 
 	public void TakeDamage(float amount)
